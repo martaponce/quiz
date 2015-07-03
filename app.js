eaa706c575f4790a3f6284aec579eb3cbac453c7
juaesto <juaesto@gmail.com>
2015-07-03 13:50:20 +0200

    Añadir link a github en el footer

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -54,6 +54,5 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
