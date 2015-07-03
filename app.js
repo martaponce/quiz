51f5459d5d743a80243ccc5bdef8555b4dcf32f2
juaesto <juaesto@gmail.com>
2015-07-03 12:43:42 +0200

    Añadir soporte a Heroku

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(partials());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(partials());
 
 app.use('/', routes);
 
