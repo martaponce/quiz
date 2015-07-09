1083283ed4bf0265961968a610f4407b8941e284
juaesto <juaesto@gmail.com>
2015-07-09 12:14:05 +0200

    Añadir Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -8,17 +8,18 @@ exports.load = function(req, res, next, quizId) {
 			req.quiz = quiz;
 			next();
 			res.render('quizes/show', { quiz: quiz});
-		} else {
-			next(new Error('No existe quizId = ' + quizId));
-		}
-  	}).catch(function(error) { next(error); });  
+		} else { next(new Error('No existe quizId=' + quizId));}
+  	}
+	).catch(function(error) { next(error); });  
 }; 
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll.then(function(quizes) {
-	res.render('quizes/index.ejs', { quizes: quizes});
-  }).catch(function(error) { next(error); });  
+  models.Quiz.findAll.then(
+	function(quizes) {
+		res.render('quizes/index.ejs', { quizes: quizes});
+  	}
+	).catch(function(error) { next(error); })  
 };
 
 // GET /quizes/:id
