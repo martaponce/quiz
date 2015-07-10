61dfb8a12234dbaa46cd07eebe84304c109224ba
juaesto <juaesto@gmail.com>
2015-07-10 09:54:37 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
-var authorController = require('../controllers/author_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: [] });
 });
 
-//Autoload de comandos con :quizId
-router.param('quizId', quizController.load); //autoload :quizId
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
 
 // Definición de rutas de /quizes
-router.get('/quizes', quizController.index);
-router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        quizController.update);
+router.delete('/quizes/:quizId(\\d+)',     quizController.delete);
 
-router.get('/author', authorController.author);
+router.get('/author',function(req,res) {res.render('author',{ autor: 'Tomás Martín' });});
 
 module.exports = router;
