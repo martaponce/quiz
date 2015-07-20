fb41c6c49f77cfcfc58518b9f83deaf3e96122ba
juaesto <juaesto@gmail.com>
2015-07-20 21:31:43 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,38 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
-// Página de entrada (home page)
+
+/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz', errors: [] });
+  res.render('index', { title: 'Quiz' ,errors: []});
 });
 
-// Autoload de comandos con :quizId
-router.param('quizId', quizController.load);  // autoload :quizId
+router.param('quizId', quizController.load); //autoload: quizId
+router.param('commentId', commentController.load);//autoload commentId
 
-// Definición de rutas de /quizes
-router.get('/quizes',                      quizController.index);
-router.get('/quizes/:quizId(\\d+)',        quizController.show);
+//Definicion de rutas de sesion
+router.get('/login', sessionController.new);//login
+router.post('/login', sessionController.create);//create session
+router.get('/logout', sessionController.destroy);//delete session
+
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new',                  quizController.new);
-router.post('/quizes/create',              quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
-router.put('/quizes/:quizId(\\d+)',        quizController.update);
-router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
+router.get('/quizes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
-router.get('/author',function(req,res) {res.render('author');});
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',sessionController.loginRequired,  commentController.publish);
 
+/*router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);*/
+router.get('/quizes/author', quizController.author);
+router.get('/quizes/profileimage', quizController.profileimage);
 module.exports = router;
