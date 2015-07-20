c331bbcfbf587740d2acec51822a5029b5287823
juaesto <juaesto@gmail.com>
2015-07-20 19:56:39 +0200

    Corrección de errores

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,6 @@ router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
-router.get('/author',function(req,res) {res.render('author');});
+//router.get('/author',function(req,res) {res.render('author');});
 
 module.exports = router;
