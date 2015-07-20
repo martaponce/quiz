5b2b14198f31cab9f456fa5bd8b3fbf45f781ab9
juaesto <juaesto@gmail.com>
2015-07-20 19:47:30 +0200

    Corrección de errores

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -7,7 +7,7 @@ exports.load = function(req, res, next, quizId) {
 			if (quiz) {
 				req.quiz = quiz;
 				next();
-			} else { next(new Error('No existe quizId=' + quizId)); }
+			} else { next(new Error('No existe quizId='+quizId)); }
 		}
 	).catch(function(error) { next(error);});
 };
